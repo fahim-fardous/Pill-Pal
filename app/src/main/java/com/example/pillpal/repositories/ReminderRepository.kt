de5b5054fb7a0dc749e2093ae015d650baac24bf
fahim-fardous <167993840+fahim-fardous@users.noreply.github.com>
2024-07-01 16:39:16 +0600

    Revert "Feature add screen"

--- app/src/main/java/com/example/pillpal/repositories/ReminderRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pillpal.repositories
-
-class ReminderRepository {
-}
--- app/src/main/java/com/example/pillpal/utils/BaseApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pillpal.utils
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BaseApplication:Application() {
-}
